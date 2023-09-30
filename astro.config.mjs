ae869808bb8e62952ef578595fa9244e8938630d
Ming-der Wang <mingderwang@gmail.com>
2023-09-30 16:49:13 +0800

    add tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from 'astro/config';
 import vercel from "@astrojs/vercel/serverless";
-
 import svelte from "@astrojs/svelte";
+
+import tailwind from "@astrojs/tailwind";
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: vercel(),
-  integrations: [svelte()]
+  integrations: [svelte(), tailwind(), react()]
 });
