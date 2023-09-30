72761b222c75a6dae182741345e9e58069455aaf
Ming-der Wang <mingderwang@gmail.com>
2023-09-30 16:25:33 +0800

    add Sass for svelte

---
+++ src/components/Counter.svelte
@@ -0,0 +1,15 @@
+<style lang="scss">
+    $font-stack: Helvetica, sans-serif;
+    $primary-color: #333;
+
+    body {
+      font: 100% $font-stack;
+      color: $primary-color;
+    }
+    .inverse {
+      background-color: #333;
+      color: rgb(202, 31, 31);
+    }
+</style>
+  
+<h1 class="inverse">Inverse Text</h1>
