a45f73276b282be4a844f6e21c60a775be0639ab
Ming-der Wang <mingderwang@gmail.com>
2023-10-10 13:03:26 +0800

    tmp

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,16 @@
 import { defineConfig } from 'astro/config';
 import vercel from "@astrojs/vercel/serverless";
 import svelte from "@astrojs/svelte";
-
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
+
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: vercel(),
+  adapter: node({
+    mode: 'middleware',
+  }), 
   integrations: [svelte(), tailwind(), react()]
 });
